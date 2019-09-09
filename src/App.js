37bc191b79adf8b06113ad8f1e6deb83787aba4d
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-09-09 15:57:33 -0700

    added change quarter button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeScore, setHomeScore] = useState(0);
 const [awayScore, setAwayScore] = useState(0);
+const [quarter, setQuarter] = useState(1);
 
   return (
     <div className="container">
@@ -37,6 +38,7 @@ const [awayScore, setAwayScore] = useState(0);
           <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
         </div>
+        
       </section>
     </div>
   );
